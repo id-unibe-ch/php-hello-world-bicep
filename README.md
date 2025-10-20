e60a16c20de63aa68f620fdcbc8cdc9f15537cb7
Michael Rolli <michael@rollis.ch>
2025-10-20 11:49:03 +0200

    feat: we have to start somewhere

---
+++ README.md
@@ -0,0 +1,6 @@
+# Azure Developer CLI (azd) Bicep Starter
+
+
+## References
+
+[GitHub Aciton Workflows for Bicep](https://github.com/Azure-Samples/bicep-github-actions/tree/main)
---
+++ src/index.php
@@ -0,0 +1,17 @@
+<?php
+
+date_default_timezone_set('Europe/Zurich');
+
+$weekdays = array(
+  'Monday' => 'Montag',
+  'Tuesday' => 'Dienstag',
+  'Wednesday' => 'Mittwoch',
+  'Thursday' => 'Donnerstag',
+  'Friday' => 'Freitag',
+  'Saturday' => 'Samstag',
+  'Sunday' => 'Sonntag'
+);
+
+echo "Hello World!<br />";
+echo sprintf("Es ist %s, der %s, Uhrzeit: %s", $weekdays[date("l")], date("d.m.Y"), date("H:i:s"));
+echo "<br />Gruss aus der Cloud";
